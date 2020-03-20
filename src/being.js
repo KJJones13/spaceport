d78ac84dd0f302a085f7f1d5a95caa0d51b262a9
Kyle Jones <rmbart395@yahoo.com>
2020-03-19 21:22:58 -0600

    Iteration 0 and 1

--- src/being.js
+++ src/being.js
@@ -1,4 +1,14 @@
 class Being {
+  constructor(name, species) {
+    this.isAlive = true;
+    this.credits = 0;
+    this.name = name;
+    this.species = species;
+  }
+
+  updateCredits(creditUpdate){
+    this.credits = this.credits + creditUpdate;
+  }
 }
 
 module.exports = Being;
